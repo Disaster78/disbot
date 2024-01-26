f28b60f1c514852b6db7cb08f1538d6fcb6b4ae7
Disaster78 <88614078+Disaster78@users.noreply.github.com>
2024-01-26 23:07:58 +0530

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -201,14 +201,14 @@ class Moderation(commands.Cog, name="Moderation"):
         await ctx.send(embed=embed)
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 1200154683721928805:
-            if not message.attachments:  # Check if the message has no attachments
+        if message.attachments and message.channel.id == 1200154683721928805:
+            await message.add_reaction("<:emoji_47:1200357003567042590>")
+            await message.add_reaction("<:emoji_47:1200357030205067354>")
+            if not message.attachments:
                 await message.delete()
-            else:
-                # Assuming correct emoji IDs, replace them with the actual IDs
-                await message.add_reaction("<:emoji_47:1200357003567042590>")
-                await message.add_reaction("<:emoji_47:1200357030205067354>")
-
+        else:
+            pass 
+                
             
 def setup(bot):
     bot.add_cog(Moderation(bot))
